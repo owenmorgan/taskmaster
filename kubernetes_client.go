92623577dc2853009aa6e86d91882cdf490c07ba
Jamie Rodway <jamie.rodway@oneiota.co.uk>
2019-02-19 11:44:58 +0000

    remove unrequired initialisation

--- kubernetes_client.go
+++ kubernetes_client.go
@@ -1,7 +1,6 @@
 package taskmaster
 
 import (
-	"k8s.io/api/batch/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -50,8 +49,7 @@ func (k8s KubernetesClient) ListCronJobs(namespace string, lbls map[string]strin
 
 // GetCronJob -
 func (k8s KubernetesClient) GetCronJob(namespace, name string) (cj *CronJob, err error) {
-	kcj := &v1beta1.CronJob{}
-	kcj, err = k8s.kubernetes.BatchV1beta1().CronJobs(namespace).Get(name, metav1.GetOptions{})
+	kcj, err := k8s.kubernetes.BatchV1beta1().CronJobs(namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return
 	}
